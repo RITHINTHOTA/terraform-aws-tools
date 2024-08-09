c2868871d91f53e9f334c50e155ce6c6f95298d8
RITHINTHOTA <thotasrisairithin@gmail.com>
2024-08-09 17:19:11 +0530

    commit

---
+++ cicd/jenkins-agent.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+yum install fontconfig java-17-openjdk -y
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+sudo yum -y install terraform
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
---
+++ cicd/variables.tf
@@ -0,0 +1,5 @@
+variable "zone_name" {
+  type        = string
+  default     = "rithinexpense.online"
+  description = "description"
+}
